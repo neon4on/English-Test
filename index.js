b2c00c78adca16b68124fa2e8beb5c1059ef299a
Valeriy Kamenskikh <86181396+neon4on@users.noreply.github.com>
2023-04-07 17:43:49 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,41 +2,77 @@
 
 let liFirst = document.createElement('li');
 let scoreBool;
-document.getElementById("clickMe").onclick = function () { 
-    let score = document.querySelector('input[name="item"]:checked');
-    let scoreValue = score.value;
-    switch(scoreValue){
-    case "Биноколь": 
-        console.log("Не верно, попробуйте ещё раз");
-        scoreBool = false;
-        liFirst.innerHTML = `<strong> ${scoreValue} </strong> - <a class="a_False">${scoreBool}</a>`;
-        ol.prepend(liFirst);
-        break;
-    case "Очки":
-        console.log("Не верно, попробуйте ещё раз");
-        scoreBool = false;
-        liFirst.innerHTML = `<strong> ${scoreValue} </strong> - <a class="a_False">${scoreBool}</a>`;
-        ol.prepend(liFirst);
-        break;
-    case "Тема": 
-        console.log("Верно");
-        scoreBool = true;
-        liFirst.innerHTML = `<strong> ${scoreValue} </strong> - <a class="a_True">${scoreBool}</a>`;
-        ol.prepend(liFirst);
-        break;
-    case "Теория":
-        console.log("Не верно, попробуйте ещё раз");
-        scoreBool = false;
-        liFirst.innerHTML = `<strong> ${scoreValue} </strong> - <a class="a_False">${scoreBool}</a>`;
-        ol.prepend(liFirst);
-        break;
-    default: 
-        console.log("Не знаю таких значений");
-        break;
-    }
-}
+let i = 1;
+
+let notRightAnswer = "He верно";
+let rightAnswer = "Верно";
 
 var form = document.getElementById("myForm");
 function handleForm(event) { event.preventDefault(); } 
-form.addEventListener('submit', handleForm);
+
+document.getElementById("clickMe").onclick = function () { 
+    form.addEventListener('submit', handleForm);
+    for (i = 1; i < 11; i++){
+        let score = document.querySelector(`input[name="item${i}"]:checked`);
+        let scoreValue = score.value;
+        if (user.step < 10){
+            switch(scoreValue){
+            case "Биноколь": 
+                console.log(notRightAnswer);
+                user.upStepNotRight();
+                user.showStep();
+                break;
+            case "Очки":
+                console.log(notRightAnswer);
+                user.upStepNotRight();
+                user.showStep();
+                break;
+            case "Тема": 
+                console.log(rightAnswer);
+                user.upStepRight();
+                user.showStep();
+                break;
+            case "Теория":
+                console.log(notRightAnswer);
+                user.upStepNotRight();
+                user.showStep();
+                break;
+            default: 
+                console.log("He знаю таких значений");
+                user.showStep();
+                break;
+            }
+        }
+    }
+    if (user.scores <= 4){
+        user.scores = 0;
+        scoreBool = "Ещё стоит подучитьcя";
+        liFirst.innerHTML = `<strong> ${user.scores} </strong> - <a class="a_False">${scoreBool}</a>`;
+    }
+    else{
+        scoreBool = "Крутой";
+        liFirst.innerHTML = `<strong> ${user.scores} </strong> - <a class="a_True">${scoreBool}</a>`;
+    }
+    ol.prepend(liFirst);
+}
+
+let user = {
+    scores: 0,
+    step: 0,
+    upStepRight() {
+        this.step++;
+        this.scores++;
+    },
+    upStepNotRight() {
+        this.step++;
+        this.scores--;
+    },
+    showStep: function() { // показывает текущий уровень вопроса
+      console.log("Шаг: " + this.step);
+      console.log("Очки: " + this.scores);
+      console.log("--------------------------------------");
+    }
+};
+
+
 
