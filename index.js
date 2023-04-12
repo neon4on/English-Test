3caa2e2976d6e4362e0a933d3be39896d6eeb440
Valeriy Kamenskikh <86181396+neon4on@users.noreply.github.com>
2023-04-12 22:27:58 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ const form = document.getElementById("myForm");
 let scoreBool;
 
 document.getElementById("clickMe").onclick = function () { 
-    const NUMBER_OF_BUTTONS = document.querySelectorAll(`input[type=radio]:checked`).length; //10
+    const NUMBER_OF_BUTTONS = document.querySelectorAll(`input[type=radio]:checked`).length; 
     form.addEventListener('submit', handleForm);
+    scoreCount(NUMBER_OF_BUTTONS);
+    userResult();
+    ol.appendChild(li);
+}
+
+const scoreCount = function(NUMBER_OF_BUTTONS){    //function expression
     for (let i = 1; i < NUMBER_OF_BUTTONS + 1; i++){
         let score = document.querySelector(`input[name="item${i}"]:checked`);
         let scoreValue = score.value;
@@ -31,6 +37,9 @@ document.getElementById("clickMe").onclick = function () {
             }
         }
     }
+}
+
+const userResult = function(){    //function expression
     if (user.scores <= 4){
         scoreBool = "Ещё стоит подучиться";
         li.innerHTML = `<strong> ${user.scores} </strong> - <a class="a_False">${scoreBool}</a>`;
@@ -38,26 +47,25 @@ document.getElementById("clickMe").onclick = function () {
     else{
         scoreBool = "Крутой";
         li.innerHTML = `<strong> ${user.scores} </strong> - <a class="a_True">${scoreBool}</a>`;
-    }
-    ol.appendChild(li);
+    } 
 }
 
-let user = { // Компонент очков и шагов пользователя
+let user = {    // Компонент очков и шагов пользователя
     scores: 0,
     step: 0,
     upStepRight() {
-        this.step++;
-        this.scores++;
+        user.step++;
+        user.scores++;
     },
     upStepNotRight() {
-        this.step++;
+        user.step++;
     },
-    showStep: function() { // Показывает текущий уровень вопроса
-      console.log("Шаг: " + this.step);
-      console.log("Очки: " + this.scores);
+    showStep: function() {    // Показывает текущий уровень вопроса
+      console.log("Шаг: " + user.step);
+      console.log("Очки: " + user.scores);
       console.log("--------------------------------------");
     }
 };
 
-function handleForm(event) { event.preventDefault(); } 
+function handleForm(event) { event.preventDefault(); }    //function declaration
 
